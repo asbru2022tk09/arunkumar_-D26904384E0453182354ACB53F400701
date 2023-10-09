f72a44e80e530b16d7cc5bc491f7ac3828530d63
asbru2022tk09 <147149441+asbru2022tk09@users.noreply.github.com>
2023-10-09 12:49:43 +0530

    main (9).py

---
+++ main (9).py
@@ -0,0 +1,21 @@
+#leap
+
+"""
+year % 4 == 0 &
+year % 100 !== 0 /
+year % 400 == 0
+
+"""
+def isLeapYear(year):
+   if (year % 4 == 0 and year % 100 != 0) or year % 400 == 0:
+     retrun True
+   else:
+     retrun False
+
+year = 2012
+
+if isLeapYear(year)
+   print('{} is not a leap year .'.format(year))
+else:
+   print('{} is not a leap year .'.format(year))
+  
